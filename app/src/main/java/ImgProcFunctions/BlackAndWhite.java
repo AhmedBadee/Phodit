0167cabf40d94d696823ebde4380554961173212
AhmedBadee <ahmedbadee17@gmail.com>
2017-07-29 04:28:13 +0200

    Black and White class

---
+++ app/src/main/java/ImgProcFunctions/BlackAndWhite.java
@@ -0,0 +1,38 @@
+package ImgProcFunctions;
+
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class BlackAndWhite {
+
+    private Bitmap selectedImageBitmap;
+    private Bitmap bitmapImageToShow;
+    public BlackAndWhite() {
+    }
+
+    public void setSelectedImage(Bitmap selectedImageBitmap) {
+        this.selectedImageBitmap = selectedImageBitmap;
+    }
+
+    public void applyConversion() {
+
+        if (selectedImageBitmap != null) {
+            Mat selectedImageMat = new Mat(selectedImageBitmap.getHeight(), selectedImageBitmap.getWidth(), CvType.CV_8UC3);
+            Utils.bitmapToMat(selectedImageBitmap, selectedImageMat);
+            Imgproc.cvtColor(selectedImageMat, selectedImageMat, Imgproc.COLOR_BGR2GRAY);
+
+            Log.e("Color Conversion", "Converted");
+            bitmapImageToShow = Bitmap.createBitmap(selectedImageMat.width(), selectedImageMat.height(), Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(selectedImageMat, bitmapImageToShow);
+        }
+    }
+
+    public Bitmap getBitmapImageToShow() {
+        return bitmapImageToShow;
+    }
+}
